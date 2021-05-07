bb872f41cb678f91d31a88b2b55c29d3293279c2
GuimaFV <guimafv@gmail.com>
2021-05-07 13:56:22 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,9 @@
 export type SaleSum = {
-    sellerName: string,
-    sum: number
+    sellerName: string;
+    sum: number;
+}
+export type SaleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
 }
